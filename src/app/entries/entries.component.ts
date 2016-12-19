16aeee2cd7c8a000ea771b274e780a0209148004
kaltenrieder <kaltenrieder@afca.ch>
2016-12-19 14:27:11 +0100

    Bugfix

--- src/app/entries/entries.component.ts
+++ src/app/entries/entries.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TimeTrackingEntry} from '../data/timeTrackingEntry';
+import { TimeTrackingEntry } from '../data/timeTrackingEntry';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 @Component({
@@ -8,7 +8,7 @@ import { Http, Response, RequestOptions, Headers } from '@angular/http';
   styleUrls: ['./entries.component.scss']
 })
 export class EntriesComponent implements OnInit {
-	rows: Array<TimeTrackingEntry> = [];
+  rows: Array<TimeTrackingEntry> = [];
   columns = [];
   editing = {};
   http: Http;
@@ -18,7 +18,7 @@ export class EntriesComponent implements OnInit {
   constructor(http: Http) {
     this.http = http;
   }
- 
+
   ngOnInit() {
     this.loadEntries();
     // this.makeRequest();
@@ -29,8 +29,8 @@ export class EntriesComponent implements OnInit {
     this.rows[row.$$index][cell] = event.target.value;
     console.log(row, cell, cellValue);
   }
-  private loadEntries() {
 
+  private loadEntries() {
     this.rows = [
       new TimeTrackingEntry('FEC', 'Add translations to tutorialservice', 6.5),
       new TimeTrackingEntry('Evita', 'Add Button to UI', 1),
@@ -40,20 +40,21 @@ export class EntriesComponent implements OnInit {
       { prop: 'project' },
       { name: 'task' },
       { name: 'time' }
-
-/*    this.columns = [
-      { prop: 'id' },
-      { name: 'Person ID' },
-      { name: 'Task ID ' },
-      { name: 'Start Date' },
-      { name: 'End Date' }
     ];
-*/
-/*    this.rows = [
-      { id: 1, personId: 2, taskId: 2, startDate: '2016-12-18', endDate: 'null' },
-      { id: 1, personId: 2, taskId: 2, startDate: '2016-12-18', endDate: 'null' }
-    ];*/
+
+    /*    this.columns = [
+          { prop: 'id' },
+          { name: 'Person ID' },
+          { name: 'Task ID ' },
+          { name: 'Start Date' },
+          { name: 'End Date' }
+    */
+    /*    this.rows = [
+          { id: 1, personId: 2, taskId: 2, startDate: '2016-12-18', endDate: 'null' },
+          { id: 1, personId: 2, taskId: 2, startDate: '2016-12-18', endDate: 'null' }
+        ];*/
   }
+
   makeRequest(): void {
     this.loading = true;
     this.http.get('http://mojito.dev.fluance.net:8080/timetracker/Entries')
