b77993b7572622cd895998363f6925ba9e0d8d93
kaltenrieder <kaltenrieder@afca.ch>
2016-12-20 15:46:13 +0100

    Layout on sidebar.

--- src/app/time-tracking/components/sidebar/sidebar.component.ts
+++ src/app/time-tracking/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,15 +6,20 @@ import { Component, ViewChild} from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  public disabled:boolean = false;
-  public status:{isopen:boolean} = {isopen: false};
-  public items:Array<string> = ['The first choice!',
+
+  public totalHousWorkedWeek: number = 12;
+  public totalHousWorkedMonth: number = 56;
+  public totalAvailableVacationDays: number = 18;
+
+  public disabled: boolean = false;
+  public status: { isopen: boolean } = { isopen: false };
+  public items: Array<string> = ['The first choice!',
     'And another choice for you.', 'but wait! A third!'];
- 
-  public toggled(open:boolean):void {
+
+  public toggled(open: boolean): void {
     console.log('Dropdown is now: ', open);
   }
-  public toggleDropdown($event:MouseEvent):void {
+  public toggleDropdown($event: MouseEvent): void {
     $event.preventDefault();
     $event.stopPropagation();
     this.status.isopen = !this.status.isopen;
