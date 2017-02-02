612ae08c015068cd6370840c9a1f71a3c17b3a0a
Ecehan Erken <eerken@fluance.ch>
2017-02-02 16:31:06 +0100

    Set names to html tags

--- src/app/time-tracking/components/entries/entry-dialog/entry-dialog.component.ts
+++ src/app/time-tracking/components/entries/entry-dialog/entry-dialog.component.ts
@@ -11,13 +11,22 @@ export class EntryDialogComponent implements OnInit {
   @Input() tasks: ITask[] = [];
 	public title: string;
   public selectedDescription: string;
+  // description: string = (<HTMLInputElement>document.getElementsByName("description")).value;
+  //public project = (<HTMLInputElement>document.getElemenstByName("project")).value; 
 
   constructor(
   	public dialogRef: MdDialogRef<EntryDialogComponent>,
-  	public projectService: ProjectService, public taskService: TaskService) { 
+  	public projectService: ProjectService, 
+    public taskService: TaskService,
+    public timeTrackingEntryService: TimeTrackingEntryService) { 
     
     // this.selectedModule = this.projects[0].projectName;
   }
+
+
+   //createEntry(){
+     // this.timeTrackingEntryService.createTimeTrackingEntry(description, projectName, taskDescriptopm, date, startTime, endTime);
+    //}
 
   ngOnInit() {
    this.projectService.getProjects().then((projects) => { this.projects = projects;
