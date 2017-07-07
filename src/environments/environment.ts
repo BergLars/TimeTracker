cf919c1f1c4b94562698eed7b6f460f542efc713
Michael Ramahenina <mramahenina@fluance.ch>
2017-07-07 08:28:57 +0200

    Change deve environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,17 @@
-export const environment = {
-	production: false,
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
 
-	title: 'Fluance Timetracker - PROD',
-	apiBaseUrl: 'https://timetracker.fluance.net/timetracker'
+export const environment = {
+  production: false,
+
+  title: 'Fluance Timetracker - DEV',
+  apiBaseUrl: 'http://localhost:8081/'
+  
+  // title: 'Fluance Timetracker - DEV',
+  //apiBaseUrl: 'https://mojito.dev.fluance.net:8443/timetracker'
+
+  // Deployment on Mojito
+  // apiBaseUrl: 'https://localhost:8443/timetracker'
 };
