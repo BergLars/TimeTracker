01b00ebecd54948868454cb3b8e4671ed8e1e3d0
Michael Ramahenina <mramahenina@fluance.ch>
2018-02-15 14:15:54 +0100

    Automated tests on create button
    task, project, client and user
    
    FTT-281

---
+++ e2e/spec/at-create.spec.js
@@ -0,0 +1,115 @@
+var Timetracker = require('../page/timetracker.js');
+var timeTracker = new Timetracker();
+var Login = require('../page/login.js');
+var login = new Login();
+var CreateComponent = require('../page/at-create.js');
+var createComponent = new CreateComponent();
+
+describe('Menu', () => {
+    beforeEach(() => {
+        timeTracker.navigateTo();
+        login.loginUser(login.username, login.password);
+    });
+    afterEach(() => {
+        browser.wait(EC.elementToBeClickable(createComponent.profileIcon), TIMEOUT, "Profile icon" + " not clickable");
+        createComponent.profileIcon.click();
+        browser.wait(EC.elementToBeClickable(createComponent.logoutButton), TIMEOUT, "Logout button" + " not clickable");
+        createComponent.logoutButton.click();
+    });
+    describe('Toolbar', () => {
+        it('Create a task as black', () => {
+            expect(createComponent.createButton.isPresent()).toBe(true);
+            browser.wait(EC.elementToBeClickable(createComponent.createButton), TIMEOUT, "Create button is not ready" + " not present");
+            createComponent.createButton.click();
+            expect(createComponent.descriptionTaskField.isPresent()).toBe(true);
+            browser.wait(EC.elementToBeClickable(createComponent.descriptionTaskField), TIMEOUT, "Description task field is not ready" + " not present");
+            createComponent.descriptionTaskField.sendKeys(createComponent.itemName);
+            browser.sleep(500);
+            createComponent.okButton.click();
+        });
+        it('Verify created task', () => {
+            browser.wait(EC.elementToBeClickable(createComponent.editButton), TIMEOUT, "Edit button is not ready" + " not present");
+            createComponent.editButton.click();
+            browser.wait(EC.elementToBeClickable(createComponent.taskOptions), TIMEOUT, "Task opitons is not ready" + " not present");
+            expect(element(by.cssContainingText('option', 'black')).isPresent()).toBe(true);
+            createComponent.cancelButton.click();
+        });
+    });
+    describe('Toolbar', () => {
+        it('Create a Project as black', () => {
+            browser.wait(EC.elementToBeClickable(createComponent.createButton), TIMEOUT, "Create button is not ready" + " not present");
+            createComponent.createButton.click();
+            browser.wait(EC.elementToBeClickable(createComponent.itemToBeCreated), TIMEOUT, "Edit button is not ready" + " not present");
+            createComponent.itemToBeCreated.click();
+            element(by.cssContainingText('option', 'Project')).click();
+            expect(createComponent.projectNameField.isPresent()).toBe(true);
+            browser.wait(EC.elementToBeClickable(createComponent.projectNameField), TIMEOUT, "Project name field is not ready" + " not present");
+            createComponent.projectNameField.sendKeys(createComponent.itemName);
+            browser.sleep(500);
+            createComponent.okButton.click();
+        });
+        it('Verify created project', () => {
+            browser.wait(EC.elementToBeClickable(createComponent.editButton), TIMEOUT, "Edit button is not ready" + " not present");
+            createComponent.editButton.click();
+            createComponent.itemToBeCreated.click();
+            element(by.cssContainingText('option', 'Project')).click();
+            browser.sleep(500);
+            createComponent.projectOptions.click();
+            expect(element(by.cssContainingText('option', 'black')).isPresent()).toBe(true);
+            createComponent.cancelButton.click();
+        });
+    });
+
+    describe('Toolbar', () => {
+        it('Create a Client as black', () => {
+            browser.wait(EC.elementToBeClickable(createComponent.createButton), TIMEOUT, "Create button is not ready" + " not present");
+            createComponent.createButton.click();
+            browser.wait(EC.elementToBeClickable(createComponent.itemToBeCreated), TIMEOUT, "Edit button is not ready" + " not present");
+            createComponent.itemToBeCreated.click();
+            element(by.cssContainingText('option', 'Client')).click();
+            browser.wait(EC.elementToBeClickable(createComponent.clientNameField), TIMEOUT, "Client name field is not ready" + " not present");
+            createComponent.clientNameField.sendKeys(createComponent.itemName);
+            expect(createComponent.clientNameField.isPresent()).toBe(true);
+            browser.sleep(500);
+            createComponent.okButton.click();
+        });
+        it('Verify created client', () => {
+            browser.wait(EC.elementToBeClickable(createComponent.editButton), TIMEOUT, "Edit button is not ready" + " not present");
+            createComponent.editButton.click();
+            createComponent.itemToBeCreated.click();
+            element(by.cssContainingText('option', 'Client')).click();
+            browser.sleep(500);
+            createComponent.clientOptions.click();
+            expect(element(by.cssContainingText('option', 'black')).isPresent()).toBe(true);
+            createComponent.cancelButton.click();
+        });
+    });
+
+    describe('Toolbar', () => {
+        it('Create a User as black', () => {
+            browser.wait(EC.elementToBeClickable(createComponent.createButton), TIMEOUT, "Create button is not ready" + " not present");
+            createComponent.createButton.click();
+            browser.wait(EC.elementToBeClickable(createComponent.itemToBeCreated), TIMEOUT, "Edit button is not ready" + " not present");
+            createComponent.itemToBeCreated.click();
+            element(by.cssContainingText('option', 'User')).click();
+            expect(createComponent.usernameField.isPresent()).toBe(true);
+            createComponent.usernameField.sendKeys(createComponent.itemName);
+            browser.sleep(500);
+            createComponent.passwordField.sendKeys(createComponent.password);
+            browser.sleep(500);
+            createComponent.confirmPasswordField.sendKeys(createComponent.password);
+            browser.sleep(500);
+            createComponent.employmentDegreeField.sendKeys(createComponent.employmentDegree);
+            browser.sleep(500);
+            createComponent.okButton.click();
+        });
+        it('Verify created user', () => {
+            browser.wait(EC.elementToBeClickable(createComponent.profileIcon), TIMEOUT, "Profile icon" + " not clickable");
+            createComponent.profileIcon.click();
+            browser.wait(EC.elementToBeClickable(createComponent.deleteUserButton), TIMEOUT, "Delete button" + " not clickable");
+            createComponent.deleteUserButton.click();
+            expect(element(by.cssContainingText('option', 'black')).isPresent()).toBe(true);
+            createComponent.cancelButton.click();
+        });
+    });
+});
--- protractor.conf.js
+++ protractor.conf.js
@@ -9,8 +9,9 @@ exports.config = {
   specs: [
     // './e2e/**/login.spec.js',
     // './e2e/**/timetracker.spec.js',
-    './e2e/**/at-menu.spec.js',
-    './e2e/**/at-export.spec.js',
+    './e2e/**/at-create.spec.js',
+    // './e2e/**/at-menu.spec.js',
+    // './e2e/**/at-export.spec.js',
     // './e2e/**/menu-toolbar.spec.js',
     // './e2e/**/entries-toolbar.spec.js'
   ],
