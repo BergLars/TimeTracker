b7463e7525059f7e55fcd8ab851c0ef6085a1aeb
Lars Christian Berg <larschristian.berg@bbzsogr.ch>
2019-03-19 14:31:48 +0100

    cleaned up a bit

--- TimeTracker-FrontEnd/src/app/component-login/component-login.component.ts
+++ TimeTracker-FrontEnd/src/app/component-login/component-login.component.ts
@@ -1,24 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero }    from '../hero';
 
 @Component({
   selector: 'app-component-login',
   templateUrl: './component-login.component.html',
   styleUrls: ['./component-login.component.scss']
 })
+
 export class ComponentLoginComponent {
-
-  powers = ['Really Smart', 'Super Flexible',
-  'Super Hot', 'Weather Changer'];
-
-  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet', 'ree');
-
   submitted = false;
-
   onSubmit() { this.submitted = true; }
-
-  newHero() {
-  this.model = new Hero(42, '', '');
-  }
-
 }
